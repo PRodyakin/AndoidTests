32d6ce772f1b14ffdb137ec572ffe1ba73a3124c
Prodyakin <prodiakin@dengisrazy.ru>
2019-09-06 16:46:27 +0300

    add desc to tests

--- src/test/java/test/RepeatedApp.java
+++ src/test/java/test/RepeatedApp.java
@@ -4,42 +4,116 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.core.annotation.Order;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.qameta.allure.Epic;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
 
 
 @Epic("Автотест для проверки повторной заявки пользователя")
 public class RepeatedApp {
-	
+
+	AndroidDriver<MobileElement> driver;
+
 	String ASSERT_ACCEPTED = "Мы оцениваем вашу\r\n" + 
 			"		платежеспособность";
-
+	
+	
 	@Test
 	public void RepeatedApptest() throws MalformedURLException{
-		AndroidDriver<MobileElement> driver = appiumInit();
+		
+		
+		appiumInit();
+		
+		
 		//caps.setCapability("automationName", "selendroid");
 		// TODO Auto-generated method stub
-		
-		afterReset(driver);
-		
+
+		afterReset();
+
 		//login
-		
-		login(driver);
-		
+
+		login();
+
 		//take
-		take(driver);
+		take();
 
 	}
 	
-	@Description("Оформление повторной заявки")
-	public void take(AndroidDriver<MobileElement> driver) {
+	
+	@Step("Инициализация")
+	public void appiumInit() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability("deviceName", "My Phone");
+		desiredCapabilities.setCapability("udid", "emulator-5554"); //Give Device ID of your mobile phone
+		desiredCapabilities.setCapability("platformName", "Android");
+		desiredCapabilities.setCapability("platformVersion", "5.1");
+		desiredCapabilities.setCapability("appPackage", "com.oneclickmoney.ocm");
+		desiredCapabilities.setCapability("appActivity", ".PreloadActivity");
+		desiredCapabilities.setCapability("noReset", "true");
+		desiredCapabilities.setCapability("unicodeKeyboard", "true");
+		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+
+	}
+	
+	
+	@Step("Шаги после первого запуска")
+	public void afterReset() {
+		driver.resetApp();
+		//after reset
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		MobileElement el23 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/next");
+		el23.click();
+		MobileElement el33 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/next");
+		el33.click();
+		el33.click();
+		el33.click();
+		el33.click();
+		MobileElement el43 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/confirm");
+		el43.click();
+	}
+	
+	
+	@Step("Вход в систему через почтовый адрес")
+	public void login() {
+		MobileElement el111 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/menuButton");
+		el111.click();
+		MobileElement el211 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/loginText");
+		el211.click();
+
+
+		driver.context("WEBVIEW");
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+		driver.findElement(By.id("reset-email")).click();
+		driver.findElement(By.id("reset-email")).sendKeys("");
+
+
+		driver.findElement(By.id("login-email")).click();
+		driver.findElement(By.id("login-email")).sendKeys("D@f.ru");
+
+		driver.findElement(By.id("pass-ent")).click();
+		driver.findElement(By.id("pass-ent")).sendKeys("12345678");
+
+		driver.findElement(By.className("btn-yellow")).click();
+	}
+	
+	
+	@Step("Оформление повторной заявки")
+	public void take() {
 		driver.context("NATIVE_APP");
 		//MobileElement el2 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/menuButton");
 		//el2.click();
@@ -58,61 +132,12 @@ public class RepeatedApp {
 		assert cleanStr(e18.getText()).equals(cleanStr(ASSERT_ACCEPTED));
 	}
 	
-	@Description("Вход в систему через почтовый адрес")
-	public void login(AndroidDriver<MobileElement> driver) {
-		MobileElement el111 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/menuButton");
-		el111.click();
-		MobileElement el211 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/loginText");
-		el211.click();
-		
-
-		driver.context("WEBVIEW");
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		
-		driver.findElement(By.id("reset-email")).click();
-		driver.findElement(By.id("reset-email")).sendKeys("");
-
-
-		driver.findElement(By.id("login-email")).click();
-		driver.findElement(By.id("login-email")).sendKeys("D@f.ru");
-
-		driver.findElement(By.id("pass-ent")).click();
-		driver.findElement(By.id("pass-ent")).sendKeys("12345678");
-
-		driver.findElement(By.className("btn-yellow")).click();
-	}
-
-	@Description("Шаги после первого запуска")
-	public void afterReset(AndroidDriver<MobileElement> driver) {
-		driver.resetApp();
-		//after reset
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		MobileElement el23 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/next");
-		el23.click();
-		MobileElement el33 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/next");
-		el33.click();
-		el33.click();
-		el33.click();
-		el33.click();
-		MobileElement el43 = (MobileElement) driver.findElementById("com.oneclickmoney.ocm:id/confirm");
-		el43.click();
-	}
 	
-	@Description("Инициализация")
-	public AndroidDriver<MobileElement> appiumInit() throws MalformedURLException {
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setCapability("deviceName", "My Phone");
-		desiredCapabilities.setCapability("udid", "emulator-5554"); //Give Device ID of your mobile phone
-		desiredCapabilities.setCapability("platformName", "Android");
-		desiredCapabilities.setCapability("platformVersion", "5.1");
-		desiredCapabilities.setCapability("appPackage", "com.oneclickmoney.ocm");
-		desiredCapabilities.setCapability("appActivity", ".PreloadActivity");
-		desiredCapabilities.setCapability("noReset", "true");
-		desiredCapabilities.setCapability("unicodeKeyboard", "true");
-		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-		return driver;
-	}
+
 	
+
+	
+
 	public static String cleanStr(String str) {
 		str = str.replaceAll("[\\W]", "");
 		str = str.replaceAll("[\\S]", "");
@@ -120,5 +145,4 @@ public class RepeatedApp {
 	}
 
 	
-
 }
